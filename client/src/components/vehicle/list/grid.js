a9fbce461706f9859b6ca4c021e3bf386f2cd362
Cristiano Pacheco <chris.spb25@gmail.com>
2018-04-28 21:10:54 -0300

    small refactory

---
+++ client/src/components/vehicle/list/grid.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { Table, Header, Segment, Button } from 'semantic-ui-react'
+
+const VehicleGrid = ({ isLoading, vehicles }) => (
+  <div>
+    <Header as='h2' attached='top'>
+      Vehicles List
+      <Link to='/add'>
+        <Button primary floated='right'>Add</Button>
+      </Link>
+    </Header>
+    <Segment attached loading={isLoading}>
+      <Table compact celled selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell width={1}>ID</Table.HeaderCell>
+            <Table.HeaderCell>Vehicles</Table.HeaderCell>
+            <Table.HeaderCell>Brand</Table.HeaderCell>
+            <Table.HeaderCell width={1} textAlign='center'>Sold</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {vehicles.map(item => (
+            <Table.Row key={item.id}>
+              <Table.Cell>{item.id}</Table.Cell>
+              <Table.Cell>
+                <a href=''>{item.vehicle}</a>
+              </Table.Cell>
+              <Table.Cell>{item.brand}</Table.Cell>
+              <Table.Cell textAlign='center'>{item.sold === '1' ? 'Yes' : 'No' }</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </Segment>
+  </div>
+)
+
+VehicleGrid.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  vehicles: PropTypes.array.isRequired
+}
+
+export default VehicleGrid
--- client/src/components/vehicle/list/index.js
+++ client/src/components/vehicle/list/index.js
@@ -1,8 +1,7 @@
 import React, { PureComponent } from 'react'
-import { Link } from 'react-router-dom'
-import { Table, Header, Segment, Button } from 'semantic-ui-react'
 
 import * as VehicleAPI from '../../../api/vehicles'
+import VehicleGrid from './grid'
 
 class VehicleList extends PureComponent {
   constructor () {
@@ -30,38 +29,10 @@ class VehicleList extends PureComponent {
 
   render () {
     return (
-      <div>
-        <Header as='h2' attached='top'>
-          Vehicles List
-          <Link to='/add'>
-            <Button primary floated='right'>Add</Button>
-          </Link>
-        </Header>
-        <Segment attached loading={this.state.isLoading}>
-          <Table compact celled selectable>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell width={1}>ID</Table.HeaderCell>
-                <Table.HeaderCell>Vehicles</Table.HeaderCell>
-                <Table.HeaderCell>Brand</Table.HeaderCell>
-                <Table.HeaderCell width={1} textAlign='center'>Sold</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {this.state.vehicles.map(item => (
-                <Table.Row key={item.id}>
-                  <Table.Cell>{item.id}</Table.Cell>
-                  <Table.Cell>
-                    <a href=''>{item.vehicle}</a>
-                  </Table.Cell>
-                  <Table.Cell>{item.brand}</Table.Cell>
-                  <Table.Cell textAlign='center'>{item.sold === '1' ? 'Yes' : 'No' }</Table.Cell>
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </Table>
-        </Segment>
-      </div>
+      <VehicleGrid
+        isLoading={this.state.isLoading}
+        vehicles={this.state.vehicles}
+      />
     )
   }
 }
